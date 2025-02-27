b21be4a631159442a8b944878a0fe93aae7d80f2
Magda Wrzosek <wrzos12mag@gmail.com>
2025-02-27 20:27:12 +0100

    small changes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/wrzos/CLionProjects/untitled4
+# Install script for directory: C:/Users/wrzos/Desktop/compression tool/Compression-tool
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/wrzos/CLionProjects/untitled4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/wrzos/Desktop/compression tool/Compression-tool/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -3,57 +3,60 @@
 #include <vector>
 #include <unordered_map>
 
+struct Node {
+    char symbol;
+    int frequency;
+    Node *left;
+    Node *right;
+
+    Node(char s, int freq) : symbol(s), frequency(freq), left(nullptr), right(nullptr) {}
+
+    //for an internal node
+    Node(int freq, Node *l, Node *r) : symbol('\0'), frequency(freq), left(l), right(r) {}
 
 
+};
+struct Compare {
+    bool operator()(Node *a, Node *b) {
+        return a->frequency > b->frequency; // Kolejka o najmniejszej wartości na szczycie
+    }
+};
+
+Node *buildHuffmanTree(const std::unordered_map<char, int> &frequencies);
 int main() {
     std::cout << "Hello, World!" << std::endl;
-
-    struct Node {
-        char symbol;
-        int frequency;
-        Node *left;
-        Node *right;
-
-        Node(char s, int freq) : symbol(s), frequency(freq), left(nullptr), right(nullptr) {}
-
-        //for an internal node
-        Node(int freq, Node *l, Node *r) : symbol('\0'), frequency(freq), left(l), right(r) {}
-
-
-    };
-
-    struct Compare {
-        bool operator()(Node *a, Node *b) {
-            return a->frequency > b->frequency; // Kolejka o najmniejszej wartości na szczycie
-        }
-    };
-
-    Node *buildHuffmanTree(const std::unordered_map<char, int> &frequencies) {
-
-        std::priority_queue<Node*, std::vector<Node *>, Compare> pq;
-
-        for (const auto &pair: frequencies) {
-            pq.push(new Node(pair.first, pair.second));
-        }
-
-        while (pq.size() > 1) {
-            // two smallest frequencies are taken from priority queue
-            Node *left = pq.top();
-            pq.pop();
-            Node *right = pq.top();
-            pq.pop();
-
-            Node *parent = new Node(left->frequency + right->frequncy, left, right);
-            pq.push(parent);
-        }
-
-        return pq.top();
+    std::string example = "aaaaabbbccccaaaaaaccc";
+    std::unordered_map<char,int> exFreqMap;
+    for (char ch : example) {
+        std::cout << ch << std::endl;
+        exFreqMap[ch]++;
     }
 
-
+    Node *exNode = buildHuffmanTree(exFreqMap);
 
     //generating Huffman codes
 
 
     return 0;
+}
+Node *buildHuffmanTree(const std::unordered_map<char, int> &frequencies) {
+
+    std::priority_queue<Node *, std::vector<Node *>, Compare> pq;
+
+    for (const auto &pair: frequencies) {
+        pq.push(new Node(pair.first, pair.second));
+    }
+
+    while (pq.size() > 1) {
+        // two smallest frequencies are taken from priority queue
+        Node *left = pq.top();
+        pq.pop();
+        Node *right = pq.top();
+        pq.pop();
+
+        Node *parent = new Node(left->frequency + right->frequency, left, right);
+        pq.push(parent);
+    }
+
+    return pq.top();
 }
