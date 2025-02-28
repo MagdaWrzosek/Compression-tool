897fa136e712bc02306a6b9a1ea5bb1e85b3c381
Magda Wrzosek <wrzos12mag@gmail.com>
2025-02-28 18:37:34 +0100

    generating Huffman codes

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ struct Node {
     Node(int freq, Node *l, Node *r) : symbol('\0'), frequency(freq), left(l), right(r) {}
 
 
+
 };
 struct Compare {
     bool operator()(Node *a, Node *b) {
@@ -23,9 +24,13 @@ struct Compare {
 };
 
 Node *buildHuffmanTree(const std::unordered_map<char, int> &frequencies);
+
+std::unordered_map<char, std::string> generateCodes(Node* root);
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::string example = "aaaaabbbccccaaaaaaccc";
+    std::string example = "aaaaabbbcccc";
     std::unordered_map<char,int> exFreqMap;
     for (char ch : example) {
         std::cout << ch << std::endl;
@@ -34,7 +39,11 @@ int main() {
 
     Node *exNode = buildHuffmanTree(exFreqMap);
 
+    //{a:5, b:3, c:4}
+
     //generating Huffman codes
+
+
 
 
     return 0;
@@ -59,4 +68,21 @@ Node *buildHuffmanTree(const std::unordered_map<char, int> &frequencies) {
     }
 
     return pq.top();
+}
+//codes are generated recursively
+std::unordered_map<char, std::string> generateCodes(Node* root, std::string code = ""){     //default arg
+    std::unordered_map<char, std::string> huffmanCodes;
+    if (root->symbol != '\0') {             //not a leaf
+        huffmanCodes [root->symbol] = code;
+        return huffmanCodes;
+    }
+    //recursive function calls itself. This ensures all the nodes on left and right side are serviced
+    std::unordered_map<char, std::string> leftMap = generateCodes(root->left, code + "0");
+    std::unordered_map<char, std::string> rightMap = generateCodes(root->right, code + "1");
+
+    huffmanCodes.insert(leftMap.begin(),leftMap.end());
+    huffmanCodes.insert(rightMap.begin(), rightMap.end());
+
+
+    return huffmanCodes;
 }
